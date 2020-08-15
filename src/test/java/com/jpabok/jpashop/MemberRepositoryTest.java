35d4ee181ed60dc631b09f5fc33d25d98f198c04
kyhslam <kyhslam@gmail.com>
2020-08-15 11:25:55 +0900

    DB Connection Test

--- src/test/java/com/jpabok/jpashop/MemberRepositoryTest.java
+++ src/test/java/com/jpabok/jpashop/MemberRepositoryTest.java
@@ -5,25 +5,29 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
-@SpringBootApplication
+@SpringBootTest
+@Transactional
 public class MemberRepositoryTest {
 
-    @Autowired MemberRepository memberRepository;
+    @Autowired
+    MemberRepository memberRepository;
 
-// //	runtimeOnly 'com.h2database:h2'
     @Test
-    @Transactional
+    @Rollback(false)
     public void testMember() throws Exception {
         // given
         Member member = new Member();
         member.setUsername("memberA");
-
 
         // when
         Long saveId = memberRepository.save(member);
@@ -32,6 +36,7 @@ public class MemberRepositoryTest {
         // then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
 
     }
 }
