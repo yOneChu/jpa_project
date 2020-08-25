28930a7283087ddf1eb77fb2d68af6b5fe645e3b
kyhslam <kyhslam@gmail.com>
2020-08-25 15:01:10 +0900

    member search dev

--- src/main/java/com/jpabok/jpashop/service/MemberService.java
+++ src/main/java/com/jpabok/jpashop/service/MemberService.java
@@ -41,7 +41,7 @@ public class MemberService {
     }
 
     //회원 전체 조회
-    private List<Member> findMembers() {
+    public List<Member> findMembers() {
         return memberRepository.findAll();
     }
 
