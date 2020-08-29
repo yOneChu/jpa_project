f24ee596ce234cf3e6036e98134033e3d1dbd152
kyhslam <kyhslam@gmail.com>
2020-08-29 18:17:05 +0900

    member update api

--- src/main/java/com/jpabok/jpashop/api/MemberApiController.java
+++ src/main/java/com/jpabok/jpashop/api/MemberApiController.java
@@ -3,11 +3,10 @@ package com.jpabok.jpashop.api;
 
 import com.jpabok.jpashop.domain.Member;
 import com.jpabok.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -23,6 +22,7 @@ public class MemberApiController {
         return new CreateMemberResponse(id);
     }
 
+    // 생성 api
     @PostMapping("/api/v2/members")
     public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
 
@@ -33,6 +33,29 @@ public class MemberApiController {
         return new CreateMemberResponse(id);
     }
 
+    // 수정 api
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(
+                            @PathVariable("id") Long id,
+                            @RequestBody @Valid UpdateMemberRequest request) {
+
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
+    }
 
     @Data
     static class CreateMemberRequest {
